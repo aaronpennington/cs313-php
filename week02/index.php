7942992fab147f207baaba96ec51d16c4df3bbd2
Aaron Pennington <b.aaron.pennington@gmail.com>
2019-09-25 22:34:18 -0600

    Initial commit

---
+++ week02/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Week 02</title>
+</head>
+
+<body> 
+  <h1> PHP TEST </h1>
+  <?php for ($i = 0; $i < 10; $i++) { ?>
+    <div id="<?php echo $i; ?>">
+  <?php} ?>
+</body>
+</html>
