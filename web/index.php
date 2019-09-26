956ceb482e05be58f0229bb05dc6af6345535b78
Aaron Pennington <b.aaron.pennington@gmail.com>
2019-09-25 23:11:22 -0600

    Update index.php

--- web/index.php
+++ web/index.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
+  <title>Aaron Pennington Homepage</title>
 </head>
 
 <body>
@@ -13,6 +13,12 @@
     <li><a href="week01/hello.html">Week 01</a></li>
     <li><a href="week02/index.php">Week 02</a></li>
   </ul>
+
+  <?php
+    $num = 128 * 256;
+    echo "128 * 256 = $num";
+    echo "<br>Look, I can do PHP!";
+  ?>
 </body>
 
 </html>
