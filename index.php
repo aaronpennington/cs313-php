cfc0a3fbb6c0ac48f8031d48f5de6ccd00c894f1
Aaron Pennington <b.aaron.pennington@gmail.com>
2019-09-25 22:41:00 -0600

    Add index to main page

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+
+<body>
+  <ul>
+    <li><a href="week01/hello.html">Week 01</a></li>
+    <li><a href="week02/index.php">Week 02</a></li>
+  </ul>
+</body>
+
+</html>
