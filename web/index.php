314e7deb8c41d58726b38d88aca278111847acc7
Aaron Pennington <b.aaron.pennington@gmail.com>
2019-09-28 21:25:27 -0600

    Move assets folder

--- web/index.php
+++ web/index.php
@@ -16,7 +16,7 @@
   ?>
   <div class="container-fluid bg-1 text-center">
     <h1 class="title">Hi, I'm <br> Aaron Pennington</h1>
-    <img src="../assets/me1000x.jpg" alt="Me" id="profilePic" class="rounded-circle"
+    <img src="./assets/me1000x.jpg" alt="Me" id="profilePic" class="rounded-circle"
       onmouseover="this.style.cursor='pointer'" onclick="imageClick()">
     <h3>I am a Computer Science student at BYU-Idaho, with a
       passion for software development and machine learning!</h3>
@@ -24,10 +24,10 @@
     <hr>
     <h3 class="contact">Contact Me</h3>
     <!--Links to Github, Email, & Linkedin here -->
-    <a href="https://www.github.com/aaronpennington"><img src="../assets/github.png" alt="Github" class="img-icons"></a>
-    <a href="https://www.linkedin.com/in/b-aaron-pennington"><img src="../assets/linkedin.png" alt="Linkedin"
+    <a href="https://www.github.com/aaronpennington"><img src="./assets/github.png" alt="Github" class="img-icons"></a>
+    <a href="https://www.linkedin.com/in/b-aaron-pennington"><img src="./assets/linkedin.png" alt="Linkedin"
         class="img-icons"></a>
-    <a href="mailto:pen17020@byui.edu"><img src="../assets/email.png" alt="Email" class="img-icons"></a>
+    <a href="mailto:pen17020@byui.edu"><img src="./assets/email.png" alt="Email" class="img-icons"></a>
 
     <footer>
       <h3>Additional Links</h3>
@@ -42,9 +42,9 @@
 
   function imageClick() {
     if (toggle === true) {
-      document.getElementById("profilePic").src = "../assets/c_h_square.jpg";
+      document.getElementById("profilePic").src = "./assets/c_h_square.jpg";
     } else {
-      document.getElementById("profilePic").src = "../assets/me1000x.jpg";
+      document.getElementById("profilePic").src = "./assets/me1000x.jpg";
     }
     toggle = !toggle;
   }
--- web/introduction.php
+++ web/introduction.php
@@ -10,7 +10,10 @@
 </head>
 
 <body>
-  <?php include 'header.php'; ?>
+  <?php 
+    $intro = "active";
+    include 'header.php'; 
+  ?>
   <div class="container-fluid">
     <h1 class="title">About Me</h1>
     <p>I am a Computer Science student at BYU-Idaho. I love developing software. In my free time, I enjoy reading,
