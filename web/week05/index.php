1f12e6757a3e4fc0e06dcd073e1feb1aa995dc9b
Aaron Pennington <b.aaron.pennington@gmail.com>
2019-10-19 17:36:25 -0600

    Update submit

--- web/week05/index.php
+++ web/week05/index.php
@@ -37,15 +37,15 @@
   </form>
 
   <div id="posts">
-    <?php 
-    foreach ($db->query('SELECT public.user.username AS user_name, public.user.display_name AS display_name, public.post.title AS title, public.post.subtitle AS subtitle, public.post.content AS content, public.post.post_date AS post_date FROM public.USER, public.POST WHERE public.USER.ID = ' . $user . ' AND public.POST.USER_ID = ' . $user . '  ORDER BY public.post.post_date;') as $row)
-    {
-      echo '<h2>' . $row['title'] . '</h2>';
-      echo '<h4>' . $row['subtitle'] . '</h4>';
-      echo '<h5> Posted by ' . $row['display_name'] . ' on ' . $row['post_date'] . '</h5>';
-      echo '<hr><br/>';
-    }
-    ?>
+    <!-- <--?php 
+    // foreach ($db->query('SELECT public.user.username AS user_name, public.user.display_name AS display_name, public.post.title AS title, public.post.subtitle AS subtitle, public.post.content AS content, public.post.post_date AS post_date FROM public.USER, public.POST WHERE public.USER.ID = ' . $user . ' AND public.POST.USER_ID = ' . $user . '  ORDER BY public.post.post_date;') as $row)
+    // {
+    //   echo '<h2>' . $row['title'] . '</h2>';
+    //   echo '<h4>' . $row['subtitle'] . '</h4>';
+    //   echo '<h5> Posted by ' . $row['display_name'] . ' on ' . $row['post_date'] . '</h5>';
+    //   echo '<hr><br/>';
+    // }
+    ?> -->
   </div>
 </body>
 
--- web/week05/submit.php
+++ web/week05/submit.php
@@ -1,5 +1,5 @@
 <?php
-  $user_id = _POST['user_id'];
+  $user_id = $_POST['user_id'];
   foreach ($db->query('SELECT public.user.username AS user_name, public.user.display_name AS display_name, public.post.title AS title, public.post.subtitle AS subtitle, public.post.content AS content, public.post.post_date AS post_date FROM public.USER, public.POST WHERE public.USER.ID = ' . $user_id . ' AND public.POST.USER_ID = ' . $user_id . '  ORDER BY public.post.post_date;') as $row)
   {
     echo '<h2>' . $row['title'] . '</h2>';
