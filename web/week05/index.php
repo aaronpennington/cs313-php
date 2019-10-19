101e72d53202e972df69f22e9aae4984683cb18d
Aaron Pennington <b.aaron.pennington@gmail.com>
2019-10-19 16:07:32 -0600

    Update index

--- web/week05/index.php
+++ web/week05/index.php
@@ -12,7 +12,7 @@
 
 <body>
   <?php 
-    foreach ($db->query('SELECT username, password FROM user') as $row)
+    foreach ($db->query('SELECT username, password FROM public.user') as $row)
     {
       echo 'user: ' . $row['username'];
       echo ' password: ' . $row['password'];
