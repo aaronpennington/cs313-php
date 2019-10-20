3ac8d8bb0d057a55a3db0080c361074516212e70
Aaron Pennington <b.aaron.pennington@gmail.com>
2019-10-19 18:20:28 -0600

    Update link href

--- web/week05/submit.php
+++ web/week05/submit.php
@@ -4,7 +4,7 @@
   
   foreach ($db->query('SELECT public.user.username AS user_name, public.post.id AS post_id, public.user.display_name AS display_name, public.post.title AS title, public.post.subtitle AS subtitle, public.post.content AS content, public.post.post_date AS post_date FROM public.USER, public.POST WHERE public.USER.ID = ' . $user_id . ' AND public.POST.USER_ID = ' . $user_id . ' ORDER BY public.post.post_date;') as $row)
   {
-    echo '<a href="post.php?id=' . $row['post_id'] . '>';
+    echo "<a href='post.php?id=" . $row['post_id'] . ">";
     echo '<h2>' . $row['title'] . '</h2>';
     echo '<h4>' . $row['subtitle'] . '</h4>';
     echo '<h5> Posted by ' . $row['display_name'] . ' on ' . $row['post_date'] . '</h5>';
