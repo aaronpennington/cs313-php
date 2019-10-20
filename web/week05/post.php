50b94fa9563f78c0f8dbfdce91708879d7b78347
Aaron Pennington <b.aaron.pennington@gmail.com>
2019-10-19 18:10:01 -0600

    Update post.php

--- web/week05/post.php
+++ web/week05/post.php
@@ -24,14 +24,15 @@
   </div>
 
   <?php 
-    foreach ($db->query('SELECT public.user.username AS user_name, public.user.display_name AS display_name, public.post.title AS title, public.post.subtitle AS subtitle, public.post.content AS content, public.post.post_date AS post_date FROM public.USER, public.POST WHERE public.USER.ID = 4 ORDER BY public.user.username;')
-  as $row)
+    $user_id = $_GET['id'];
+    foreach ($db->query('SELECT public.user.username AS user_name, public.user.display_name AS display_name, public.post.title AS title, public.post.subtitle AS subtitle, public.post.content AS content, public.post.post_date AS post_date FROM public.USER, public.POST WHERE public.USER.ID = 4 ORDER BY public.user.username;') as $row)
     {
-      echo '<h2>' . $row['title'] . '</h2>';
-      echo '<h4>' . $row['subtitle'] . '</h4>';
+      echo '<h1>' . $row['title'] . '</h2>';
+      echo '<h3>' . $row['subtitle'] . '</h3>';
+      echo '<p>' . $row['content'] . '</p>';
       echo '<h5> Posted by ' . $row['display_name'] . ' on ' . $row['post_date'] . '</h5>';
-      echo '<hr><br/>';
     }
+
   ?>
 </body>
 
--- web/week05/scripts.js
+++ web/week05/scripts.js
@@ -1,12 +1,10 @@
 function submitFormData() {
   let user_id = $("#userList").val();
-  console.log("USER ID: " + user_id);
 
   $.post("submit.php", {
       user_id: user_id
     },
     function (data) {
       $('#posts').html(data);
-      console.log(data);
     });
 }
