515897d7dc69ef2e16004f9f1bb59cf15c78b35d
Aaron Pennington <b.aaron.pennington@gmail.com>
2019-10-19 18:54:34 -0600

    Update header

--- web/week05/header.php
+++ web/week05/header.php
@@ -1,35 +1,34 @@
-<div class="jumbotron">
-  <h1>Susan Bush</h1>
-  <p>Susan is a teacher for VIP Kids</p>
-</div>
+<header>
+  <div class="jumbotron">
+    <h1>Susan Bush</h1>
+    <p>Susan is a teacher for VIP Kids</p>
+  </div>
 
-<div id="nav">
-  <div class="navbar navbar-inverse navbar-static-top">
-    <div class="container">
-      <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
-      <a href="index.php" class="navbar-brand">Home</a>
-      <a class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </a>
-      <div class="navbar-collapse collapse">
-        <ul class="nav navbar-nav">
-          <li><a href="#">About Me</a></li>
-          <li><a href="#">All Posts</a></li>
-        </ul>
-        <ul class="nav pull-right navbar-nav">
-          <li>
-            <form class="navbar-form">
-              <input type="text" class="form-control" placeholder="Search">
-              <button type="submit" class="btn btn-default"><i class="glyphicon glyphicon-search"></i></button>
-            </form>
-          </li>
-          <li>
-            <a href="#"><span class="badge">2</span></a>
-          </li>
-        </ul>
+  <div id="nav">
+    <div class="navbar navbar-inverse navbar-static-top">
+      <div class="container">
+        <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
+        <a href="index.php" class="navbar-brand">Home</a>
+        <a class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </a>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li><a href="#">About Me</a></li>
+            <li><a href="#">All Posts</a></li>
+          </ul>
+          <ul class="nav pull-right navbar-nav">
+            <li>
+              <form class="navbar-form">
+                <input type="text" class="form-control" placeholder="Search">
+                <button type="submit" class="btn btn-default"><i class="glyphicon glyphicon-search"></i></button>
+              </form>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-  </div><!-- /.navbar -->
-</div>
+    </div><!-- /.navbar -->
+  </div>
+</header>
