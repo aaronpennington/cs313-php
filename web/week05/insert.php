0933ccdd1a6fdd884b3d6e68116ee5e3d505a4e3
Aaron Pennington <b.aaron.pennington@gmail.com>
2019-10-25 20:17:12 -0600

    Update redirect in insert

--- web/week05/insert.php
+++ web/week05/insert.php
@@ -31,10 +31,10 @@ catch (Exception $ex)
 }
 
 // // finally, redirect them to a new page to actually show the topics
-// $post_id = $db->query('SELECT id FROM public.POST ORDER BY id DESC LIMIT 1;')
-// header("Location: post.php?postId=$post_id");
+$post_id = $db->query('SELECT id FROM public.POST ORDER BY id DESC LIMIT 1;')
+header("Location: post.php?postId=$post_id");
 
-// die(); // we always include a die after redirects. In this case, there would be no
+die(); // we always include a die after redirects. In this case, there would be no
 //        // harm if the user got the rest of the page, because there is nothing else
 //        // but in general, there could be things after here that we don't want them
 //        // to see.
