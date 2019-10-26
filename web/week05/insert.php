639bb8bb3b49b92fa64a0aabe32391c7d8e733f5
Aaron Pennington <b.aaron.pennington@gmail.com>
2019-10-25 20:40:06 -0600

    Test redirect

--- web/week05/insert.php
+++ web/week05/insert.php
@@ -31,8 +31,9 @@ catch (Exception $ex)
 
 // // finally, redirect them to a new page to actually show the topics
 $post_id = $db->query('SELECT id FROM public.POST ORDER BY id DESC LIMIT 1;');
-$url = "post.php?id=".$post_id;
-header("Location: $url");
+echo $post_id;
+// $url = "post.php?id=".$post_id;
+// header("Location: ".$url);
 
 exit(); // we always include a die after redirects. In this case, there would be no
 //        // harm if the user got the rest of the page, because there is nothing else
