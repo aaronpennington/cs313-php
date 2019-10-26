319f8c992b8f54cc6e47a371d7118f3206d46853
Aaron Pennington <b.aaron.pennington@gmail.com>
2019-10-25 19:05:34 -0600

    Update navbar

--- web/includes/header.php
+++ web/includes/header.php
@@ -4,31 +4,25 @@
     <p>Susan is a teacher for VIP Kids</p>
   </div>
 
-  <div id="nav">
-    <div class="navbar navbar-inverse navbar-static-top">
-      <div class="nContainer">
-        <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
-        <a href="index.php" class="navbar-brand">Home</a>
-        <a class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </a>
-        <div class="navbar-collapse collapse">
-          <ul class="nav navbar-nav">
-            <li><a href="#">About Me</a></li>
-            <li><a href="#">All Posts</a></li>
-          </ul>
-          <ul class="nav pull-right navbar-nav">
-            <li>
-              <form class="navbar-form">
-                <input type="text" class="form-control" placeholder="Search">
-                <button type="submit" class="btn btn-default"><i class="glyphicon glyphicon-search"></i></button>
-              </form>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div><!-- /.navbar -->
-  </div>
+  <nav class="navbar navbar-expand-md bg-dark navbar-dark">
+    <!-- Brand -->
+    <a class="navbar-brand" href="../week05/index.php">Home</a>
+
+    <!-- Toggler/collapsibe Button -->
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <!-- Navbar links -->
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="#">About Me</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
 </header>
