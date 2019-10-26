1ff06c3ff00d863d6886d832cf9a272a193af290
Aaron Pennington <b.aaron.pennington@gmail.com>
2019-10-26 17:03:37 -0600

    testing

--- web/includes/header.php
+++ web/includes/header.php
@@ -34,4 +34,13 @@
         </ul>
       </div>
     </nav>
+    <div>
+      <?php 
+        if ( isset( $_SESSION['user_id'] ) ) {
+          echo "<p>Logged in as: ".$_SESSION['user_id']."</p>";
+      } else {
+          echo "<p>Not Signed In</p>";
+      }
+      ?>
+    </div>
 </header>
