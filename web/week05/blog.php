bae33a9c769e8794002eeae693a3bd96d3e1d3d1
Aaron Pennington <b.aaron.pennington@gmail.com>
2019-10-25 19:14:01 -0600

    Update blog form

--- web/week05/blog.php
+++ web/week05/blog.php
@@ -24,7 +24,7 @@ include "../includes/links.php" ?>
       <div class="form-row">
         <div class="form-group col-md-6">
           <label for="inputContent">Post Text</label>
-          <input type="textarea" class="form-control" name="inputContent">
+          <input type="textarea" rows=10 class="form-control" name="inputContent">
         </div>
       </div>
       <button type="submit" class="btn btn-primary">Submit Post</button>
