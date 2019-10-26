32b194ca75e9b03e43ea629098eb1ce41001b67e
Aaron Pennington <b.aaron.pennington@gmail.com>
2019-10-26 16:35:44 -0600

    testing

--- web/week05/submit_login.php
+++ web/week05/submit_login.php
@@ -2,7 +2,27 @@
 // Always start this first
 session_start();
 
-include "../includes/db.sql";
+try
+{
+  $dbUrl = getenv('DATABASE_URL');
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
 include "../includes/links.php";
 
 echo $_POST['username'];
@@ -13,7 +33,7 @@ if ( ! empty( $_POST ) ) {
       // Getting submitted user data from database
       $query = 'SELECT * FROM public.USER WHERE public.USER.username=\':username\';';
       echo $query;
-      $statement = $db->prepare('SELECT * FROM public.USER WHERE public.USER.username=\':username\';');
+      $statement = $db->prepare($query);
       $statement->bindValue(':username', $username, PDO::PARAM_STR);
       $statement->execute();
       echo "DONE";
