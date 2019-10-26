48a91f87dc7e72fce2f9677649a0c23471f36ffe
Aaron Pennington <b.aaron.pennington@gmail.com>
2019-10-26 16:05:05 -0600

    testing

--- web/week05/submit_login.php
+++ web/week05/submit_login.php
@@ -19,7 +19,7 @@ if ( ! empty( $_POST ) ) {
 //     	// Verify user password and set $_SESSION
 //     	// if ( password_verify( $_POST['password'], $user->password ) ) {
 //     	// 	$_SESSION['user_id'] = $user->ID;
-    	}
+    	// }
     }
 
     // header("Location: index.php");
