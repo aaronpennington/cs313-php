a30b55aefaef9d706c022c848b63366ee46ed0b6
Aaron Pennington <b.aaron.pennington@gmail.com>
2019-10-26 15:59:13 -0600

    testing

--- web/week05/submit_login.php
+++ web/week05/submit_login.php
@@ -5,16 +5,16 @@ session_start();
 include "../includes/db.sql";
 echo $_POST['username'];
 echo $_POST['password'];
-// if ( ! empty( $_POST ) ) {
-//     if ( isset( $_POST['username'] ) && isset( $_POST['password'] ) ) {
-//       // Getting submitted user data from database
-//       $query = 'SELECT * FROM public.USERS WHERE username=:username');
-//       $statement = $db->prepare($query);
-//       $statement->bindValue(':username', $_POST['username']);
-//       $statement->execute();
-//       $results = $statement->fetchAll(PDO::FETCH_ASSOC);
-//       // $user = $results->fetchObject();
-//       echo $results;
+if ( ! empty( $_POST ) ) {
+    if ( isset( $_POST['username'] ) && isset( $_POST['password'] ) ) {
+      // Getting submitted user data from database
+      $query = 'SELECT * FROM public.USERS WHERE username=:username');
+      $statement = $db->prepare($query);
+      $statement->bindValue(':username', $_POST['username']);
+      $statement->execute();
+      // $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+      // // $user = $results->fetchObject();
+      // echo $results;
     		
 //     	// Verify user password and set $_SESSION
 //     	// if ( password_verify( $_POST['password'], $user->password ) ) {
@@ -24,5 +24,5 @@ echo $_POST['password'];
 
     // header("Location: index.php");
     // exit();
-// }
+}
 ?>
