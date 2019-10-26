82208b7ceef13777c0e5445e46d8915b63bd0560
Aaron Pennington <b.aaron.pennington@gmail.com>
2019-10-26 17:56:21 -0600

    testing

--- web/week05/logout.php
+++ web/week05/logout.php
@@ -5,4 +5,7 @@ session_start();
 // Destroying the session clears the $_SESSION variable, thus "logging" the user
 // out. This also happens automatically when the browser is closed
 session_destroy();
+
+header("Location: index.php");
+exit();
 ?>
