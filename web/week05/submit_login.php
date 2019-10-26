9e964501986c0418225882591f4dccd2b24031a7
Aaron Pennington <b.aaron.pennington@gmail.com>
2019-10-26 15:51:00 -0600

    update login

--- web/week05/submit_login.php
+++ web/week05/submit_login.php
@@ -6,12 +6,13 @@ include "../includes/db.sql";
 
 if ( ! empty( $_POST ) ) {
     if ( isset( $_POST['username'] ) && isset( $_POST['password'] ) ) {
-        // Getting submitted user data from database
-        $stmt = $db->prepare("SELECT * FROM public.users WHERE username = :username");
-        $stmt->bind_param(':username', $_POST['username']);
-        $stmt->execute();
-        $result = $stmt->get_result();
-    	$user = $result->fetch_object();
+      // Getting submitted user data from database
+      $query = 'SELECT * FROM public.USERS WHERE username=:username');
+      $statement = $db->prepare($query);
+      $statement->bindValue(':username', $_POST['username']);
+      $stmt->execute();
+      $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+      $user = $results->fetchObject();
     		
     	// Verify user password and set $_SESSION
     	if ( password_verify( $_POST['password'], $user->password ) ) {
