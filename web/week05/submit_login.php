816516d9f33bee169c64dddc823c53ef5e011e11
Aaron Pennington <b.aaron.pennington@gmail.com>
2019-10-26 15:56:14 -0600

    testing

--- web/week05/submit_login.php
+++ web/week05/submit_login.php
@@ -3,25 +3,25 @@
 session_start();
 
 include "../includes/db.sql";
-
-if ( ! empty( $_POST ) ) {
-    if ( isset( $_POST['username'] ) && isset( $_POST['password'] ) ) {
-      // Getting submitted user data from database
-      $query = 'SELECT * FROM public.USERS WHERE username=:username');
-      $statement = $db->prepare($query);
-      $statement->bindValue(':username', $_POST['username']);
-      $statement->execute();
-      $results = $statement->fetchAll(PDO::FETCH_ASSOC);
-      // $user = $results->fetchObject();
-      echo $results;
+echo "HELLO";
+// if ( ! empty( $_POST ) ) {
+//     if ( isset( $_POST['username'] ) && isset( $_POST['password'] ) ) {
+//       // Getting submitted user data from database
+//       $query = 'SELECT * FROM public.USERS WHERE username=:username');
+//       $statement = $db->prepare($query);
+//       $statement->bindValue(':username', $_POST['username']);
+//       $statement->execute();
+//       $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+//       // $user = $results->fetchObject();
+//       echo $results;
     		
-    	// Verify user password and set $_SESSION
-    	// if ( password_verify( $_POST['password'], $user->password ) ) {
-    	// 	$_SESSION['user_id'] = $user->ID;
-    	// }
-    }
+//     	// Verify user password and set $_SESSION
+//     	// if ( password_verify( $_POST['password'], $user->password ) ) {
+//     	// 	$_SESSION['user_id'] = $user->ID;
+//     	// }
+//     }
 
-    // header("Location: index.php");
-    // exit();
-}
+//     // header("Location: index.php");
+//     // exit();
+// }
 ?>
