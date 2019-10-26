389ae1c88f1e60a10ac0bcc44aaa1fcd763a182b
Aaron Pennington <b.aaron.pennington@gmail.com>
2019-10-26 16:24:59 -0600

    testing

--- web/week05/submit_login.php
+++ web/week05/submit_login.php
@@ -10,7 +10,7 @@ echo $_POST['password'];
 if ( ! empty( $_POST ) ) {
     if ( isset( $_POST['username'] ) && isset( $_POST['password'] ) ) {
       // Getting submitted user data from database
-      $query = 'SELECT * FROM public.USER WHERE public.USER.username=\'admin\'';
+      $query = 'SELECT * FROM public.USER WHERE public.USER.username=\':username\'';
       echo $query;
       $statement = $db->prepare($query);
       $statement->bindValue(':username', $_POST['username'], PDO::PARAM_STR);
