0a117d89aababa02b23e345b22124d7374693041
Aaron Pennington <b.aaron.pennington@gmail.com>
2019-10-28 23:34:03 -0600

    Update post editing

--- web/week05/scripts.js
+++ web/week05/scripts.js
@@ -8,3 +8,24 @@ function submitFormData() {
       $('#posts').html(data);
     });
 }
+
+function editPost() {
+  // Replace tags with text inputs using AJAX
+  let post_title = $("#post_title").val();
+  let post_subtitle = $("#post_subtitle").val();
+  let post_text = $("#post_text").val();
+
+  $("#post_title").remove();
+
+  var edit_title = document.createElement("input");
+  edit_title.setAttribute("type", "text");
+  edit_title.setAttribute("value", post_title);
+  edit_title.setAttribute("id", "post_title");
+  $("#post_div").prepend(edit_title);
+}
+
+function editComment() {
+
+
+
+}
