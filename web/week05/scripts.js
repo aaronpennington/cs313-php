e22468aea8799de653b0556b6a15c23715bfa5b9
Aaron Pennington <b.aaron.pennington@gmail.com>
2019-10-28 23:45:34 -0600

    Update post editing

--- web/week05/scripts.js
+++ web/week05/scripts.js
@@ -16,12 +16,31 @@ function editPost() {
   let post_text = $("#post_text").text();
 
   $("#post_title").remove();
+  $("#post_subtitle").remove();
 
   var edit_title = document.createElement("input");
   edit_title.setAttribute("type", "text");
   edit_title.setAttribute("value", post_title);
   edit_title.setAttribute("id", "post_title");
   $("#post_div").prepend(edit_title);
+
+  var label_title = document.createElement("label");
+  label_title.setAttribute("for", "post_title");
+  label_title.setAttribute("value", "Title: ");
+
+  var edit_subtitle = document.createElement("input");
+  edit_subtitle.setAttribute("type", "text");
+  edit_subtitle.setAttribute("value", post_subtitle);
+  edit_subtitle.setAttribute("id", "post_subtitle");
+  $("#post_div").prepend(edit_subtitle);
+
+  var edit_text = document.createElement("textarea");
+  edit_text.setAttribute("value", post_text);
+  edit_text.setAttribute("id", "post_text");
+  edit_text.setAttribute("rows", "10");
+  edit_text.setAttribute("cols", "75");
+  $("#post_div").prepend(edit_text);
+
 }
 
 function editComment() {
