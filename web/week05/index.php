2f87df21e32c948a6b3205200c95ab83f9c5c929
Aaron Pennington <b.aaron.pennington@gmail.com>
2019-10-31 17:03:44 -0600

    Update recent posts

--- web/week05/index.php
+++ web/week05/index.php
@@ -34,16 +34,15 @@
       </div>
       <div class="row">
         <?php 
-          $statement = $db->query('SELECT public.POST.id AS post_id, public.POST.title AS title, public.POST.subtitle AS subtitle, public.POST.content as content, public.POST.post_date AS post_date, public.USER.display_name AS display_name FROM public.POST, public.USER WHERE public.POST.user_id = public.USER.id ORDER BY public.POST.id DESC LIMIT 3;');
-          
-          $row = $statement->fetch(PDO::FETCH_ASSOC);
-          echo '<div class="card" style="width: 25rem;">
-          <div class="card-body">';
-          echo '<h5 class="card-title"><a href="post.php?id='. $row['post_id'] . '">' . $row['title'] . '</a></h5>';
-          echo '<h6 class="card-subtitle mb-2 text-muted">' . $row['subtitle'] . '</h6>';
-          echo '<p class="card-text">' . $row['content'] . '</p>';
-          echo '<h6> Posted by ' . $row['display_name'] . ' on ' . $row['post_date'] . '</h6>';
-          echo '</div></div>';
+          foreach($db->query('SELECT public.POST.id AS post_id, public.POST.title AS title, public.POST.subtitle AS subtitle, public.POST.content as content, public.POST.post_date AS post_date, public.USER.display_name AS display_name FROM public.POST, public.USER WHERE public.POST.user_id = public.USER.id ORDER BY public.POST.id DESC LIMIT 3;') as $row){
+            echo '<div class="card" style="width: 25rem;">
+            <div class="card-body">';
+            echo '<h5 class="card-title"><a href="post.php?id='. $row['post_id'] . '">' . $row['title'] . '</a></h5>';
+            echo '<h6 class="card-subtitle mb-2 text-muted">' . $row['subtitle'] . '</h6>';
+            echo '<p class="card-text">' . $row['content'] . '</p>';
+            echo '<h6> Posted by ' . $row['display_name'] . ' on ' . $row['post_date'] . '</h6>';
+            echo '</div></div>';
+          }
           ?>
       </div>
     </div>
